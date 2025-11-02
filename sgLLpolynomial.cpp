bb22842ffaf014b019ccb1d09539bd01fe5820ed
tanyarai5446 <tr5335074@gmail.com>
2025-11-02 15:14:35 +0530

    .

--- sgLLpolynomial.cpp
+++ sgLLpolynomial.cpp
@@ -8,27 +8,32 @@ struct polynode
    int coef,pow;
    polynode *next;
 };
-polynode *first,*temp,*ttemp,*poly1,*poly2,*sum,*diff,*prod;
+polynode *first,*temp,*ttemp,*poly1,*poly2,*sum,*diff,*prod,*result,*a,*b;
 void init()
-{first=temp=ttemp=null; }
-void insertdata(polynode* &head,int coef,int pow)
-{ 
-  init();
-  ttemp=new polynode{coef,pow,null};
-  if (!head || pow > head->pow) {//if list empty or pow is greater
-    ttemp->next=head;
-    head=ttemp;
-  }
-  else//otherwise find correct position
-  temp=head;
-  while(temp->next&&temp->next->pow>pow)
-  temp=temp->next;
-  if(temp->pow==pow)
-  temp->coef+=coef;
-  else{
-    ttemp->next=temp->next;
-    temp->next=ttemp;
-  }
+{first=temp=ttemp=poly1=poly2=null; }
+void insertdata(polynode*& head, int coef, int pow) {
+    ttemp = new polynode{coef, pow, nullptr};
+
+    if (!head || pow > head->pow) {//if head==null i.e. emty list or inserted pow is > head->pow
+        ttemp->next = head;
+        head = ttemp;
+        return;
+    }
+
+    temp = head;
+    while (temp) {
+        if (temp->pow == pow) { //when pow = 
+            temp->coef += coef;
+            return;
+        }
+        if (!temp->next || temp->next->pow < pow)//when pow small so find correct position untill temp->!=null 
+                                                 ////or temp jisko point krra uska next ka pow kam na ho given pow se
+            break;// it will break while as soon as we encounter a  pow < given pow and we will be at the position after which we have to insert now pow           
+        temp = temp->next;
+    }
+
+    ttemp->next = temp->next;
+    temp->next = ttemp;
 }
 void disp(polynode* head)
 {
@@ -41,32 +46,84 @@ void disp(polynode* head)
 }
 polynode* addPoly(polynode*p1,polynode*p2)
 {
-
+   result=null;
+   while(p1||p2)
+   {
+    if(!p2||(p1&&p1->pow>p2->pow)){
+    insertdata(result,p1->coef,p1->pow);
+    p1=p1->next;
+   }
+   else if(!p1&&(p2&&p2->pow>p1->pow)){
+    insertdata(result,p2->coef,p2->pow);
+    p2=p2->next;
+   }
+   else{
+    insertdata(result,p2->coef+p1->coef,p1->pow);
+    p1=p1->next;
+    p2=p2->next;
+   }
+   }
+   return result;
 }
 polynode* subPoly(polynode*p1,polynode*p2)
 {
+  result=null;
+  while(p1||p2)
+  {
+    if(!p2&&(p1&&p1->pow>p2->pow))
+    {
+      insertdata(result,p1->coef,p1->pow);
+      p1=p1->next;
+   }
+   else if(!p1&&(p2&&p2->pow>p1->pow)){
+      insertdata(result,-p2->coef,p2->pow);
+      p2=p2->next;
+   }
+   else{
+      insertdata(result,p1->coef-p2->coef,p1->pow);
+      p1=p1->next;
+      p2=p2->next;
+   }
+   }
+   return result;
+    
+  }
 
-}
 polynode* mulPoly(polynode*p1,polynode*p2)
 {
-
+    result = null;
+    for ( a = p1; a != null; a = a->next) {
+        for (b = p2; b != null; b = b->next) {
+            int newCoef = a->coef * b->coef;
+            int newPow = a->pow + b->pow;
+            insertdata(result, newCoef, newPow);
+        }
+    }
+    return result;
 }
 
 int main()
-{
+{   init();
     int n,coef,pow;
     //insert poly1
     cout<<"enter no. of terms in poly1: ";
     cin>>n;
-    cout<<"\nenter terms as coef and pow";
-    cin>>coef>>pow;
-    insertdata(poly1,coef,pow);
+    cout<<"\nenter terms as coef and pow\n";
+    for (int i = 0; i < n; ++i) {
+    cin >> coef >> pow;
+    insertdata(poly1, coef, pow);
+    }
+
     //insert poly2
     cout<<"enter no. of terms in poly2: ";
     cin>>n;
-    cout<<"\nenter terms as coef and pow";
-    cin>>coef>>pow;
-    insertdata(poly2,coef,pow);
+    cout<<"\nenter terms as coef and pow\n";
+    for (int i = 0; i < n; ++i) {
+    cin >> coef >> pow;
+    insertdata(poly2, coef, pow);
+    }
+
+
     //display original
     cout << "\nPolynomial 1: ";
     disp(poly1);
@@ -77,7 +134,6 @@ int main()
      sum = addPoly(poly1, poly2);
      diff = subPoly(poly1, poly2);
      prod = mulPoly(poly1, poly2);
-
     // Display results
     cout << "\nSum: ";
     disp(sum);
@@ -85,6 +141,7 @@ int main()
     disp(diff);
     cout << "Product: ";
     disp(prod);
+
 
     return 0;
 
