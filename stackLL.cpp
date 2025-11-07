1b4acec969053cf8f5f154c328eb9bfba1dfc60c
tanyarai5446 <tr5335074@gmail.com>
2025-11-07 20:50:37 +0530

    stack LL implementation

---
+++ stackLL.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+struct Lstack
+{
+    int data;
+    Lstack *next;
+};
+Lstack *top,*temp,*ttemp;
+void init()
+{
+    top=temp=ttemp=nullptr;
+}
+void push(int x)
+{
+    temp=new Lstack;
+    if(temp==nullptr)return;
+    temp->data=x;
+    temp->next=top;
+    top=temp;
+}
+int pop()
+{
+    int y;
+    if(top==nullptr)return 0;
+    y=top->data;
+    temp=top;
+    top=top->next;
+    temp->next=nullptr;
+    delete temp;
+    return y;
+
+}
+int main()
+{
+    init();
+    push(10);
+    push(20);
+    push(30);
+    push(40);
+    cout<<"popped elements are: \n";
+    cout<<pop()<<endl;
+    cout<<pop()<<endl;
+    cout<<pop();
+}
