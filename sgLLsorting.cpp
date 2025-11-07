2a21780c6f121280cef4b2cbd063ebf790636dc9
tanyarai5446 <tr5335074@gmail.com>
2025-11-07 21:33:59 +0530

    sg LL sorting

---
+++ sgLLsorting.cpp
@@ -0,0 +1,55 @@
+#include<iostream>
+using namespace std;
+struct node
+{
+    int data;
+    node*next;
+};
+node*first,*temp,*ttemp,*p;
+void addnodesortedly()
+{
+  ttemp=new node;
+  cin>>ttemp->data;
+  ttemp->next=nullptr;
+  if(!first)
+  {
+    first=ttemp;
+    return;
+  }
+ if(ttemp->data>first->data)
+  {
+    ttemp->next=first;
+    first=ttemp;
+    return;
+  }
+  temp=first;
+  while(temp->next && temp->next->data>ttemp->data)
+      {
+        temp=temp->next;
+      }
+      ttemp->next=temp->next;
+      temp->next=ttemp;
+
+}
+void disp()
+{
+    temp=first;
+    while(temp!=nullptr)
+    {
+        cout<<temp->data<<endl;
+        temp=temp->next;
+    }
+}
+int main()
+{
+    first=temp=ttemp=nullptr;
+    int n;
+    cout<<"enter number of elements in LL: ";
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        addnodesortedly();
+    }
+    cout<<"sorted LL\n";
+    disp();
+}
