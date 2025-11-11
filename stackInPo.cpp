8262ccc80ea3eb1e41fb3cea23628d2192f793aa
tanyarai5446 <tr5335074@gmail.com>
2025-11-11 21:55:21 +0530

    stack infix to postfix

---
+++ stackInPo.cpp
@@ -0,0 +1,115 @@
+// stack polish notation conversion infix to postfix
+#include<iostream>
+using namespace std;
+struct inpo
+{
+  char *data;
+  int top,size;
+   void init(int n)
+   {
+   size=n;
+   data=new char[size];
+   top=-1;
+   }
+   void push(char c)
+   {
+    if(top==size-1)
+    return;
+    data[++top]=c;
+   }
+   char pop()
+    {
+    if(top==-1)
+    return 0;
+    return data[top--];
+    }
+}; 
+int getlength(char s[])
+{
+int i=0;
+while(s[i]!='\0')
+i++;    
+return i;
+}
+int main()
+{
+ inpo z;
+ char a[100],str[100],i=0,j=0;
+ fgets(a,100,stdin);
+ int len=getlength(a)-1;
+ z.init(len);
+ while(i<len)
+ {
+     if(a[i]=='(')
+     {
+         z.push(a[i]);
+     }
+     else if((a[i]>='A'&&a[i]<='Z')||(a[i]>='a'&&a[i]<='z'))
+     {
+         str[j]=a[i];
+         j++ ; 
+     }
+     else if(a[i]=='^')
+     {
+         z.push(a[i]);
+     }
+     else if(a[i]=='*'||a[i]=='/')
+     {
+       if(z.data[z.top]=='('||z.data[z.top]=='+'||z.data[z.top]=='-'||z.top==-1)
+       { 
+         z.push(a[i]);
+       }
+       else if(a[i]=='*'&& (z.data[z.top]=='^'||z.data[z.top]=='/'))
+       {
+         while(z.data[z.top]!='('&&z.data[z.top]!='+'&&z.data[z.top]!='-')
+         {str[j]=z.pop();j++;}
+         z.push(a[i]);
+       }
+       else if(a[i]=='/'&& (z.data[z.top]=='^'||z.data[z.top]=='*'))
+       {
+         while(z.data[z.top]!='('&&z.data[z.top]!='+'&&z.data[z.top]!='-')
+         {str[j]=z.pop();j++;}
+         z.push(a[i]);
+       }
+     }
+     else if(a[i]=='+'||a[i]=='-')
+     { 
+       if(z.data[z.top]=='('||z.top==-1)
+       { 
+         z.push(a[i]);
+       }
+       else if(a[i]=='+'&&(z.data[z.top]=='^'||z.data[z.top]=='*'||z.data[z.top]=='/'||z.data[z.top]=='-'))
+       {  
+         while(z.data[z.top]!='(')
+         {str[j]=z.pop();j++;}
+         z.push(a[i]);
+       }
+       else if(a[i]=='-'&& (z.data[z.top]=='^'||z.data[z.top]=='*'||z.data[z.top]=='/'||z.data[z.top]=='+'))
+       {
+         while(z.data[z.top]!='(')
+         {str[j]=z.pop();j++;}
+         z.push(a[i]);
+       }
+     }
+     else if(a[i]==')')
+     {
+      while(z.data[z.top]!='(')
+      {
+      str[j]=z.pop();
+      j++;
+      }
+      z.pop();
+     }
+     i++;
+    }
+    while(z.top != -1)
+    {
+     str[j++] = z.pop();
+    }
+    int leng=getlength(str);
+    for(int k=0;k<leng;k++)
+    {
+    cout<<str[k];
+    }
+    
+ }
