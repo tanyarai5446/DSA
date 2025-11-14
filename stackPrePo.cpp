81e8998ba78b939c5d3cecee3393602c35ddfac1
tanyarai5446 <tr5335074@gmail.com>
2025-11-14 14:18:19 +0530

    stack prefix to postfix

---
+++ stackPrePo.cpp
@@ -0,0 +1,74 @@
+//stack prefix to postfix
+#include<iostream>
+#include<cctype>
+#include<string>
+#include <algorithm>
+using namespace std;
+struct inpo
+{
+  std::string *data;
+  int top,size;
+   void init(int n)
+   {
+   size=n;
+   data=new std::string[size];
+   top=-1;
+   }
+   void push(std::string c)
+   {
+    if(top==size-1)
+    return;
+    data[++top]=c;
+   }
+   std::string pop()
+    {
+    if(top==-1)
+    return "";
+    return data[top--];
+    }
+}; 
+int presi(char c)
+{
+  switch(c)
+  {
+    case '^':return 3;
+    case '*':return 2;
+    case '/':return 2;
+    case '+':return 1;
+    case '-':return 1;
+    default:return 0;
+  }
+
+}
+int main()
+{
+ inpo z;
+ std::string a;
+ std::cin >> a;  // Reads until space or newline
+ std::reverse(a.begin(),a.end());
+ int len=a.length();
+ z.init(len);
+ std::string s[4];
+ int i=0;
+ while(i<len)
+{
+  if(isalnum(a[i]))
+  {
+    z.push(std::string(1,a[i]));
+  }
+  else if(presi(a[i])>0)
+  {
+    s[0]=z.pop();
+    s[1]=z.pop();
+    s[2]=a[i];
+    s[3] =  s[0] + s[1] + s[2] ;
+    z.push(s[3]);
+  }
+  i++;
+}
+
+if (z.top != -1) {
+        cout << "Postfix expression: " << z.pop() << endl;
+    }
+
+}
