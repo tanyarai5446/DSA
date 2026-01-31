70a4867f86bab220b917bf315dab4f3da91cd4c6
tanyarai5446 <tr5335074@gmail.com>
2026-01-31 21:40:09 +0530

    doubly LL poly

--- dbLLpoly.cpp
+++ dbLLpoly.cpp
@@ -74,10 +74,12 @@ poly * addpoly(poly*p1,poly*p2)
             insertdata(result,-p2->coef,p2->pow);
             p2=p2->next;
         }
-        else{
+        else
+        {
             insertdata(result,p1->coef-p2->coef,p1->pow);
             p1=p1->next;
-            p2=p2->next;}
+            p2=p2->next;
+        }
     }
     while (p1) 
     {
